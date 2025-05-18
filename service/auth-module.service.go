1c31865f7729f886d2d88017287bcb3ae2c56868
arnokay <arnokay.work@gmail.com>
2025-05-18 23:24:37 +0300

    feat(auth-module.service): add AuthProviderGet and AuthProviderUpdateTokens

--- service/auth-module.service.go
+++ service/auth-module.service.go
@@ -30,11 +30,11 @@ func NewAuthModuleService(mb *nats.Conn) *AuthModuleService {
 
 func (s *AuthModuleService) UserSessionValidate(ctx context.Context, token string) (bool, error) {
 	req := mbtypes.AuthSessionTokenRequest{
-		Data: token,
-    TraceID: trace.FromContext(ctx),
+		Data:    token,
+		TraceID: trace.FromContext(ctx),
 	}
 
-  reqBytes, _ := req.Encode()
+	reqBytes, _ := req.Encode()
 
 	msg, err := s.mb.RequestWithContext(ctx, topics.AuthSessionTokenValidate, reqBytes)
 	if err != nil {
@@ -44,65 +44,102 @@ func (s *AuthModuleService) UserSessionValidate(ctx context.Context, token strin
 
 	var response mbtypes.AuthSessionTokenValidateResponse
 
-  err = response.Decode(msg.Data)
-
-  if err != nil {
-    s.logger.ErrorContext(ctx, "cannot decode response", "err", err)
-    return false, errs.ErrInternal
-  }
+	err = response.Decode(msg.Data)
+	if err != nil {
+		s.logger.ErrorContext(ctx, "cannot decode response", "err", err)
+		return false, errs.ErrInternal
+	}
 
 	if !response.Success {
-		return false, errs.FromCode(response.Code)
+		return false, errs.New(response.Code, response.Error, nil)
 	}
 
 	return true, nil
 }
 
 func (s *AuthModuleService) AuthSessionExchange(ctx context.Context, token string) (*data.User, error) {
-  req := mbtypes.AuthSessionTokenRequest{
-    TraceID: trace.FromContext(ctx),
-    Data: token,
-  }
+	req := mbtypes.AuthSessionTokenRequest{
+		TraceID: trace.FromContext(ctx),
+		Data:    token,
+	}
 
-  b, _ := req.Encode()
+	b, _ := req.Encode()
 
-  msg, err := s.mb.RequestWithContext(ctx, topics.AuthSessionTokenExchange, b)
-  if err != nil {
-    s.logger.ErrorContext(ctx, "cannot request user session exchange", "err", err)
-    return nil, errs.ErrInternal
-  }
+	msg, err := s.mb.RequestWithContext(ctx, topics.AuthSessionTokenExchange, b)
+	if err != nil {
+		s.logger.ErrorContext(ctx, "cannot request user session exchange", "err", err)
+		return nil, errs.ErrInternal
+	}
 
-  var res mbtypes.AuthSessionTokenExchangeResponse
+	var res mbtypes.AuthSessionTokenExchangeResponse
 
-  err = res.Decode(msg.Data)
-  if err != nil {
-    s.logger.ErrorContext(ctx, "cannot decode response", "err", err)
-    return nil, errs.ErrInternal
-  }
+	err = res.Decode(msg.Data)
+	if err != nil {
+		s.logger.ErrorContext(ctx, "cannot decode response", "err", err)
+		return nil, errs.ErrInternal
+	}
 
-  if !res.Success {
-    return nil, errs.FromCode(res.Code)
-  }
+	if !res.Success {
+		return nil, errs.New(res.Code, res.Error, nil)
+	}
 
-  return res.Data, nil
+	return res.Data, nil
 }
 
+func (s *AuthModuleService) AuthProviderGet(ctx context.Context, data data.AuthProviderGet) (*data.AuthProvider, error) {
+	req := mbtypes.AuthProviderGetRequest{
+		TraceID: trace.FromContext(ctx),
+		Data:    data,
+	}
 
+	b, _ := req.Encode()
 
+	msg, err := s.mb.RequestWithContext(ctx, topics.AuthProviderTokenGet, b)
+	if err != nil {
+		s.logger.ErrorContext(ctx, "cannot request provider", "err", err)
+		return nil, errs.ErrInternal
+	}
 
+	var res mbtypes.AuthProviderGetResponse
 
+	err = res.Decode(msg.Data)
+	if err != nil {
+		s.logger.ErrorContext(ctx, "cannot decode provider response", "err", err)
+		return nil, errs.ErrInternal
+	}
 
+	if !res.Success {
+		return nil, errs.New(res.Code, res.Error, nil)
+	}
 
+	return res.Data, nil
+}
 
+func (s *AuthModuleService) AuthProviderUpdateTokens(ctx context.Context, data data.AuthProviderUpdateTokens) error {
+	req := mbtypes.AuthProviderUpdateTokensRequest{
+		TraceID: trace.FromContext(ctx),
+		Data:    data,
+	}
 
+	b, _ := req.Encode()
 
+	msg, err := s.mb.RequestWithContext(ctx, topics.AuthProviderTokenUpdateTokens, b)
+	if err != nil {
+		s.logger.ErrorContext(ctx, "cannot request provider tokens update", "err", err)
+		return errs.ErrInternal
+	}
 
+	var res mbtypes.AuthProviderUpdateTokensResponse
 
+	err = res.Decode(msg.Data)
+	if err != nil {
+		s.logger.ErrorContext(ctx, "cannot decode provider response", "err", err)
+		return errs.ErrInternal
+	}
 
+	if !res.Success {
+		return errs.New(res.Code, res.Error, nil)
+	}
 
-
-
-
-
-
-
+	return nil
+}
