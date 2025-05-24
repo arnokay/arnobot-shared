07e372177cb6689886ce497b39210d1dec7d0d56
arnokay <arnokay.work@gmail.com>
2025-05-24 17:50:29 +0300

    feat: add AttachTraceID and ErrHandler middlewares

--- middlewares/middlewares.go
+++ middlewares/middlewares.go
@@ -1,26 +1,45 @@
 package middlewares
 
 import (
-	"log/slog"
+	"arnobot-shared/pkg/errs"
+	"arnobot-shared/trace"
+	"context"
+	"errors"
+	"net/http"
 
-	"arnobot-shared/applog"
-	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 )
 
-func TraceIDMiddleware() echo.MiddlewareFunc {
-  return func(next echo.HandlerFunc) echo.HandlerFunc {
-    return func(c echo.Context) error {
-      traceID := uuid.New().String()
-     
-      c.Set("trace_id", traceID)
+func AttachTraceID(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		traceID := trace.New()
 
-      logger := applog.NewWithAttrs(slog.String("trace_id", traceID))
+		c.Set(trace.TraceIDKey.String(), traceID)
+		ctx2 := context.WithValue(c.Request().Context(), trace.TraceIDKey, traceID)
+		c.SetRequest(c.Request().WithContext(ctx2))
+		c.Response().Header().Add("X-Trace-ID", traceID)
 
-      c.Set("logger", logger)
-      c.Response().Header().Set("X-Trace-ID", traceID)
+		return next(c)
+	}
+}
 
-      return next(c)
-    }
-  }
+func ErrHandler(err error, c echo.Context) {
+	if c.Response().Committed {
+		return
+	}
+	status := http.StatusInternalServerError
+  responseErr := errs.ErrInternal
+
+	var appErr *errs.AppError
+	if errors.As(err, &appErr) {
+		status = errs.ToHTTPStatus(appErr)
+    responseErr = *appErr
+	} else {
+		if he, ok := err.(*echo.HTTPError); ok {
+			status = he.Code
+      responseErr = errs.New(errs.CodeHTTP, he.Error(), he)
+		}
+	}
+
+	c.JSON(status, responseErr)
 }
