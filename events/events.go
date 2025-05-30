933ea5150e65ef4e7386eda02c54b387debc96eb
arnokay <arnokay.work@gmail.com>
2025-05-30 23:05:13 +0300

    chore: group common properties of events into EventCommon

--- events/events.go
+++ events/events.go
@@ -1,18 +1,29 @@
 package events
 
-type EventMessage struct {
-	Platform string `json:"platform"`
-	BotID    string `json:"botId"`
+type EventCommon struct {
+	Platform      string `json:"platform"`
+	BotID         string `json:"botId"`
+	BroadcasterID string `json:"broadcasterId"`
+}
+
+type Message struct {
+	EventCommon
 
 	MessageID string `json:"messageId"`
 	Message   string `json:"message"`
-	ReplyTo   string `json:"replyTo"`
+	ReplyTo   string `json:"replyTo,omitempty"`
 
-	BroadcasterID    string `json:"broadcasterId"`
 	BroadcasterLogin string `json:"broadcasterLogin"`
 	BroadcasterName  string `json:"broadcasterName"`
 
 	ChatterID    string `json:"chatterId"`
 	ChatterLogin string `json:"chatterLogin"`
 	ChatterName  string `json:"chatterName"`
+}
+
+type MessageSend struct {
+	EventCommon
+
+	Message string `json:"message"`
+	ReplyTo string `json:"replyTo,omitempty"`
 }
